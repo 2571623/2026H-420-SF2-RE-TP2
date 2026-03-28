c1cd448f87d75a7966b2d07215fa609f6f518077
Larry <lquach0511@gmail.com>
2026-03-28 16:49:46 -0400

    Version 3 - Classe Sin - Compléter

--- fonctions.py
+++ fonctions.py
@@ -5,7 +5,14 @@ class Sin(Expression):
     """Expression representant sin(u)."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, u: Expression):
+        self.u = u
+
+    def evaluer(self, x: float) -> float:
+        return Cos(self.u.evaluer(x)) * self.u.evaluer(x)
+    
+    def deriver(self) -> Expression:
+        return Sin(Cos(self.u.deriver(), self.u.deriver()))
 
 
 class Cos(Expression):
