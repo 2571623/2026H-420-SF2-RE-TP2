7e4ac0a693f2987fc01d7f79c11a6c11bd26b2ea
Larry <lquach0511@gmail.com>
2026-03-28 16:43:54 -0400

    Version 2 - Classe Addition et Multiplication  - Compléter

--- operations.py
+++ operations.py
@@ -5,11 +5,27 @@ class Addition(Expression):
     """Expression representant u + v."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, u: Expression, v: Expression):
+        self.u = u
+        self.v = v
+
+    def evaluer(self, x: float) -> float:
+        return self.u.evaluer(x) + self.v.evaluer(x)
+    
+    def deriver(self) -> "Expression":
+        return Addition(self.u.deriver(), self.v.deriver())
 
 
 class Multiplication(Expression):
     """Expression representant u * v."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, u: Expression, v: Expression):
+        self.u = u
+        self.v = v
+    
+    def evaluer(self, x: float) -> float:
+        return self.u.evaluer(x) * self.v.evaluer(x)
+    
+    def deriver(self) -> "Expression":
+        return Multiplication(self.u.deriver(), self.v.deriver())
